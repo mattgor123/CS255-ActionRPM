416e9d9da82b544412801d53e32c1cd48f9cd0a2
Carlo <kingcarlo1@gmail.com>
2014-09-16 21:36:02 -0400

    added diagnol movement

--- player.py
+++ player.py
@@ -7,6 +7,11 @@ class player(game.sprite.Sprite):
 	left = None
 	up = None
 	down = None
+	upright = None
+	upleft = None
+	downright = None
+	downleft = None
+
 	#Cannot be a decimal
 	speed = 1
 	direction = "right"
@@ -20,32 +25,56 @@ class player(game.sprite.Sprite):
 			player.left  = game.transform.rotate(player.right, 180)
 			player.up  = game.transform.rotate(player.right, 90)
 			player.down  = game.transform.rotate(player.right, -90)
+			player.upright  = game.transform.rotate(player.right, 45)
+			player.upleft  = game.transform.rotate(player.right, 135)
+			player.downright  = game.transform.rotate(player.right, -45)
+			player.downleft  = game.transform.rotate(player.right, -135)
 
 		self.image = player.image
 
 		self.rect = self.image.get_rect()
-		self.rect.topleft = [300,0]
+		self.rect.topleft = location
 
 	def move(self):	
 		#self.rect = self.rect.move(-5,0)
 		keysPressed = game.key.get_pressed()
-		if keysPressed[game.K_LEFT]:
+		if keysPressed[game.K_LEFT] and keysPressed[game.K_UP]:
+			self.rect =  self.rect.move(-self.speed,-self.speed)
+			if self.direction != "upleft":
+				self.image = player.upleft
+				self.direction = "upleft"
+		elif keysPressed[game.K_LEFT] and keysPressed[game.K_DOWN]:
+			self.rect =  self.rect.move(-self.speed,+self.speed)
+			if self.direction != "downleft":
+				self.image = player.downleft
+				self.direction = "downleft"
+		elif keysPressed[game.K_LEFT]:
 			self.rect =  self.rect.move(-self.speed,0)
 			if self.direction != "left":
-				self.image = self.left
+				self.image = player.left
 				self.direction = "left"
-		if keysPressed[game.K_RIGHT]:
+		elif keysPressed[game.K_RIGHT] and keysPressed[game.K_UP]:
+			self.rect =  self.rect.move(self.speed,-self.speed)
+			if self.direction != "upright":
+				self.image = player.upright
+				self.direction = "upright"
+		elif keysPressed[game.K_RIGHT] and keysPressed[game.K_DOWN]:
+			self.rect =  self.rect.move(self.speed,self.speed)
+			if self.direction != "downright":
+				self.image = player.downright
+				self.direction = "downright"
+		elif keysPressed[game.K_RIGHT]:
 			self.rect =  self.rect.move(self.speed,0)
 			if self.direction != "right":
-				self.image = self.right
+				self.image = player.right
 				self.direction = "right"
-		if keysPressed[game.K_UP]:
+		elif keysPressed[game.K_UP]:
 			self.rect =  self.rect.move(0,-self.speed)
 			if self.direction != "up":
-				self.image = self.up
+				self.image = player.up
 				self.direction = "up"
-		if keysPressed[game.K_DOWN]:
+		elif keysPressed[game.K_DOWN]:
 			self.rect =  self.rect.move(0,self.speed)
 			if self.direction != "down":
-				self.image = self.down
+				self.image = player.down
 				self.direction = "down"
