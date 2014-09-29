65b2c51cc9ba9714fa13fb6894bcf3815653f5c1
Matt Gorelik <matthewgorelik@gmail.com>
2014-09-29 15:34:57 -0400

    Adding README to commit

--- submit.sh
+++ submit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "$1"
-echo rm *.tar.gz
-echo find . -name "*.pyc" -print0 | xargs -0 rm -rf
-echo find . -name "*.*~" -print0 | xargs -0 rm -rf
+echo ls
+
+find . -name "*.tar.gz" -print0 | xargs -0 rm -rf
+find . -name "*.pyc" -print0 | xargs -0 rm -rf
+find . -name "*.*~" -print0 | xargs -0 rm -rf
+find . -name "*~" -print0 | xargs -0 rm -rf
+
 mkdir cs255-assignment-$1-actionrpm
 cp -r audio cs255-assignment-$1-actionrpm
 cp -r states cs255-assignment-$1-actionrpm
@@ -10,13 +14,8 @@ cp -r images cs255-assignment-$1-actionrpm
 cp -r sprites cs255-assignment-$1-actionrpm
 cp -r util cs255-assignment-$1-actionrpm
 cp game.py cs255-assignment-$1-actionrpm
+cp README cs255-assignment-$1-actionrpm
 
-cd cs255-assignment-$1-actionrpm
-
-tar -zcvf cs255-assignment-$1-actionrpm.tar.gz *
-
-cp cs255-assignment-$1-actionrpm.tar.gz ..
-
-cd ..
+tar -zcvf cs255-assignment-$1-actionrpm.tar.gz cs255-assignment-$1-actionrpm
 
 rm -rf cs255-assignment-$1-actionrpm
