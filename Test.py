a05040b40329b66f23cff2ec9251f9d7870c762b
Matt Gorelik <matthewgorelik@gmail.com>
2014-10-11 18:23:00 -0400

    Made test a lot less laggy

--- Test.py
+++ Test.py
@@ -8,25 +8,29 @@ WIDTH = 800
 HEIGHT = 600
 SCREEN = display.set_mode((WIDTH, HEIGHT))
 test = Map.Map()
-
+SCREEN.fill((0, 0, 0))
 x = 80
 y = 60
+background = PG.Surface(SCREEN.get_size())
+
 while True:
-    SCREEN.fill((0, 0, 0))
 
     group = test.render(x, y)
     group.update()
     group.draw(SCREEN)
-    display.flip()
+    display.update()
+    group.clear(SCREEN,background)
 
     for event in PE.get():
         if event.type == PG.QUIT:
             exit()
-        if event.type == PG.KEYDOWN and event.key == PG.K_RIGHT:
-            x += 1
-        if event.type == PG.KEYDOWN and event.key == PG.K_LEFT:
-            x -= 1
-        if event.type == PG.KEYDOWN and event.key == PG.K_DOWN:
-            y += 1
-        if event.type == PG.KEYDOWN and event.key == PG.K_UP:
-            y -= 1
+
+    keys_pressed = PG.key.get_pressed()
+    if keys_pressed[PG.K_RIGHT]:
+        x += 1
+    if keys_pressed[PG.K_LEFT]:
+        x -= 1
+    if keys_pressed[PG.K_DOWN]:
+        y += 1
+    if keys_pressed[PG.K_UP]:
+        y -= 1
